9bce6c85607be96d9277dcd051c4856be9be5a37
decodinatorX <decodinator@gmail.com>
2022-12-27 20:59:34 -0500

    Literally Changed One Fuckin Thing >:( and it works now

--- src/main/java/com/decodinator/liroth/core/items/PotestiumHelmetItem.java
+++ src/main/java/com/decodinator/liroth/core/items/PotestiumHelmetItem.java
@@ -13,9 +13,10 @@ import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
+import software.bernie.geckolib3.item.GeoArmorItem;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 
-public class PotestiumHelmetItem extends ArmorItem implements IAnimatable  {
+public class PotestiumHelmetItem extends GeoArmorItem implements IAnimatable  {
     private AnimationFactory factory = GeckoLibUtil.createFactory(this);
     
     public PotestiumHelmetItem(ArmorMaterial materialIn, EquipmentSlot slot, Item.Properties builder)  {
